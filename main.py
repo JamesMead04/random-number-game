e9c02953d341b7c2b5f5beb34c36b32399ecfe5a
James Mead <117443666+JamesMead04@users.noreply.github.com>
2022-11-04 12:11:11 +0000

    optimised 1

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import random
 def random_function():
+  guesses = 0
   number = random.randint(0,100)
   guess = -1
   message = "guess a number 1 to 100"
   while guess != number:
+    guesses += 1
     guess = int(input(message))
     if guess > number:
       message = "Lower"
     elif guess < number:
       message = "Higher"
     else:
-      message = "well done you got it"
-  print(message)
+      message = "well done you got it in ",guesses," guesses"
+  return message
 
-random_function()
+print(random_function())
 
   
 
